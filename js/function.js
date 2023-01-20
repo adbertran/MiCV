067da32194d2b52fbb536be699393746e618da22
adbertran <adbertran@hotmail.com>
2023-01-20 15:18:24 -0300

    Correcciones en el comportamiento

--- js/function.js
+++ js/function.js
@@ -1,28 +1,16 @@
 
- /* document.getElementById('boton_info').addEventListener("click",function(){
-    console.log ("Agregue un evento click ");
-    
-    document.body.style.backgroundColor = 'grey';
-    
-    window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200");
+document.getElementById('boton_enviar').addEventListener("click", function () {
 
-}); */
+    let nombre = document.getElementById('formulario_nombre').value;
+    let correo = document.getElementById('formulario_email').value;
+    let mensaje = document.getElementById('formulario_mensaje').value;
 
-document.getElementById('boton_enviar').addEventListener("click", function(){
+    if (nombre === "" || correo === "" || mensaje === "") {
+        alert("Informacion incompleta.\n Por favor complete toda la informacion. Muchas Gracias.");
+    }
+    else {
+        alert("Muchas Gracias " + nombre + ".\n Lo antes posible me pondre en contacto para resolver su consulta.");
+    }
 
-console.log ("funciona el boton");
-
-alert("Muchas Gracias. Lo antes posible me pondre en contacto para resolver su consulta.");
-
-
-
-
-
-
-alert("Estimado "+ document.getElementById('formulario_nombre').val + "Monto de la venta (Sin IVA): $"+montoventa+"\nImpuesto al Valor Agregado(IVA): $"+iva+"\nTotal a pagar: $"+totalpagar);
-
-/* document.formulario_contacto.submit(); */
-
-    
-
+    /* document.formulario_contacto.submit(); */
 });
