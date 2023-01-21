92c90262e4fc48cf9c337fe10627ddfa343f5e36
adbertran <adbertran@hotmail.com>
2023-01-21 10:38:34 -0300

    Revision del javascript

--- js/function.js
+++ js/function.js
@@ -1,16 +1,18 @@
+document.getElementById("boton_enviar").addEventListener("click", function () {
+  const nombre = document.getElementById("formulario_nombre").value;
+  const correo = document.getElementById("formulario_email").value;
+  const mensaje = document.getElementById("formulario_mensaje").value;
 
-document.getElementById('boton_enviar').addEventListener("click", function () {
-
-    let nombre = document.getElementById('formulario_nombre').value;
-    let correo = document.getElementById('formulario_email').value;
-    let mensaje = document.getElementById('formulario_mensaje').value;
-
-    if (nombre === "" || correo === "" || mensaje === "") {
-        alert("Informacion incompleta.\n Por favor complete toda la informacion. Muchas Gracias.");
-    }
-    else {
-        alert("Muchas Gracias " + nombre + ".\n Lo antes posible me pondre en contacto para resolver su consulta.");
-    }
-
+  if (nombre == "" || correo == "" || mensaje == "") {
+    alert(
+      "Informacion incompleta.\n Por favor complete toda la informacion. Muchas Gracias."
+    );
+  } else {
+    alert(
+      "Muchas Gracias " +
+        nombre +
+        ".\n Lo antes posible me pondre en contacto para resolver su consulta."
+    );
     /* document.formulario_contacto.submit(); */
+  }
 });
